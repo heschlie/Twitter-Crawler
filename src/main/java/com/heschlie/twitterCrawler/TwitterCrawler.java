43eed155ec570fe819dfdb2d7cdd27bd0c9cae68
Stephen Schlie <heschlie@gmail.com>
2014-08-24 22:06:20 -0700

    trying out log4j2, adding in serializing the username map

--- src/main/java/com/heschlie/twitterCrawler/TwitterCrawler.java
+++ src/main/java/com/heschlie/twitterCrawler/TwitterCrawler.java
@@ -1,7 +1,15 @@
 package com.heschlie.twitterCrawler;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.serializers.MapSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import twitter4j.Status;
-
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -17,12 +25,15 @@ public class TwitterCrawler {
 
     private int NUMTHREADS = Runtime.getRuntime().availableProcessors();
 
+    private Logger log;
+
     public static void main(String[] args) {
         TwitterCrawler twitterCrawler = new TwitterCrawler();
         twitterCrawler.init();
     }
 
     private void init() {
+        log = LogManager.getLogger();
         readTweets = new LinkedBlockingQueue<Status>();
         postTweets = new LinkedBlockingQueue<Status>();
 
@@ -35,6 +46,33 @@ public class TwitterCrawler {
         for (int i = 0; i < NUMTHREADS; i++) {
             new Thread(new TweetProcessor(readTweets, postTweets, noisyUsers, popularHashtags)).start();
         }
+
+        Timer timer = new Timer();
+//        timer.scheduleAtFixedRate(new MapWriter(), 300000L, 300000L);
+        timer.scheduleAtFixedRate(new MapWriter(), 5000L, 5000L);
     }
 
+    private class MapWriter extends TimerTask {
+        public MapWriter() {
+        }
+
+        @Override
+        public void run() {
+            try {
+                writeData();
+            } catch (FileNotFoundException e) {
+                log.error("Could not write file!");
+                log.error(e.getMessage());
+            }
+        }
+
+        private void writeData() throws FileNotFoundException {
+            Kryo kryo = new Kryo();
+            kryo.register(ConcurrentHashMap.class, new MapSerializer());
+            Output file = new Output(new FileOutputStream("userMap.bin"));
+            kryo.writeObject(file, noisyUsers);
+            file.close();
+            log.debug("File written to userMap.bin");
+        }
+    }
 }
