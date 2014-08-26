b16b02fca547b4b241ed95fe74b765314270718f
Stephen Schlie <heschlie@gmail.com>
2014-08-25 18:03:59 -0700

    adding in deserializing data

--- src/main/java/com/heschlie/twitterCrawler/TwitterCrawler.java
+++ src/main/java/com/heschlie/twitterCrawler/TwitterCrawler.java
@@ -1,11 +1,17 @@
 package com.heschlie.twitterCrawler;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.serializers.DefaultSerializers;
+import com.esotericsoftware.kryo.serializers.FieldSerializer;
 import com.esotericsoftware.kryo.serializers.MapSerializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import twitter4j.Status;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Timer;
@@ -40,6 +46,8 @@ public class TwitterCrawler {
         noisyUsers = new ConcurrentHashMap<String, AtomicInteger>();
         popularHashtags = new ConcurrentHashMap<String, AtomicInteger>();
 
+        readFiles();
+
         new Thread(new Crawler(readTweets)).start();
         NUMTHREADS--;
 
@@ -53,27 +61,68 @@ public class TwitterCrawler {
         timer.scheduleAtFixedRate(new MapWriter(), 5000L, 5000L);
     }
 
+    private void readFiles() {
+        Kryo kryo = new Kryo();
+        kryo.register(ConcurrentHashMap.class);
+        MapSerializer map = new MapSerializer();
+        map.setKeyClass(String.class, new DefaultSerializers.StringSerializer());
+        map.setValueClass(AtomicInteger.class, new FieldSerializer(kryo, AtomicInteger.class));
+
+        File users = new File("userMap.bin");
+        if (users.exists()) {
+            try {
+                Input in = new Input(new FileInputStream(users));
+                noisyUsers = kryo.readObject(in, ConcurrentHashMap.class);
+            } catch (FileNotFoundException e) {
+                log.warn(e.getMessage());
+            }
+        }
+
+        File hashTags = new File("hashTags.bin");
+        if (hashTags.exists()) {
+            try {
+                Input in = new Input(new FileInputStream(hashTags));
+                popularHashtags = kryo.readObject(in, ConcurrentHashMap.class);
+            } catch (FileNotFoundException e) {
+                log.warn(e.getMessage());
+            }
+        }
+    }
+
     private class MapWriter extends TimerTask {
+        private Kryo kryo;
+
         public MapWriter() {
+            kryo = new Kryo();
+            kryo.register(ConcurrentHashMap.class, new MapSerializer());
+            log.info("MapWriter created");
         }
 
         @Override
         public void run() {
             try {
-                writeData();
+                writeUsers();
+                writeHashTags();
             } catch (FileNotFoundException e) {
                 log.error("Could not write file!");
                 log.error(e.getMessage());
             }
         }
 
-        private void writeData() throws FileNotFoundException {
-            Kryo kryo = new Kryo();
-            kryo.register(ConcurrentHashMap.class, new MapSerializer());
+        private void writeUsers() throws FileNotFoundException {
             Output file = new Output(new FileOutputStream("userMap.bin"));
             kryo.writeObject(file, noisyUsers);
+            file.flush();
             file.close();
             log.debug("File written to userMap.bin");
+        }
+
+        private void writeHashTags() throws FileNotFoundException {
+            Output file = new Output(new FileOutputStream("hashTags.bin"));
+            kryo.writeObject(file, popularHashtags);
+            file.flush();
+            file.close();
+            log.debug("File written to hashTags.bin");
         }
     }
 }
